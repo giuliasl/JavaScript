add31f345ca4f3a3f72d8ef5623c6622fe57d064
Giulia Lima <giuliasantoslima6@gmail.com>
2024-07-29 20:17:53 -0300

    JS simples exercises

---
+++ test.js
@@ -0,0 +1,33 @@
+//Mostre um alerta com a mensagem "Boas vindas ao nosso site
+alert('Boas vindas ao site');
+
+//Declare uma variável chamada nome e atribua a ela o valor "Lua"
+let name = 'Lua';
+
+//Crie uma variável chamada idade e atribua a ela o valor 25
+let age = 25;
+
+//Defina uma variável numeroDeVendas e atribua a ela o valor 5
+let salesNumber = 5;
+
+//Defina uma variável saldoDisponivel e atribua a ela o valor 1000
+let availableSaleBalance = 1000;
+
+//Exiba um alerta com o texto "Erro! Preencha todos os campos"
+alert('Error! Fill all the fields');
+
+//Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro
+let errorMessage = 'Error! Fill all the fields';
+alert(errorMessage);
+
+//Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome
+let userName = prompt('Complete the field with your name');
+
+//Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade
+let userAge = prompt('Complete th filed with your age');
+
+//Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!"
+let userAgeNumber;
+if (userAgeNumber >= 18){
+    alert('Able to obtain a license driver =)');
+}
