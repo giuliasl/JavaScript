4a57ed82521d48d4134cdd11f235cb799859c187
Giulia Lima <giuliasantoslima6@gmail.com>
2024-07-29 21:41:23 -0300

    More exercises

---
+++ userWelcoming.js
@@ -0,0 +1,4 @@
+//Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome
+
+let userName = prompt ("Enter your name in the field");
+alert(`Hello  ${userName}, Wellcome! I hope you're well =) `);
